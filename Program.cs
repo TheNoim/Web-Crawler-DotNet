2a04edc25082174a88875e34f07291081e651815
Nils Bergmann <nilsbergmann@noim.io>
2016-10-24 12:29:39 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WebCrawlerDotNet
+{
+    internal class Program
+    {
+        public static Regex linkParser = new Regex(@"\b(?:https?://|www\.)\S+\b",
+            RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        public static Regex linkParser2 = new Regex(@"http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?",
+            RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        public static List<string> links = new List<string>();
+        public static int currentthreads = 0;
+        public static int currentindex = 0;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("[Main Thread] Bootstrap Application.");
+            Console.WriteLine("[Main Thread] Processors: " + Environment.ProcessorCount);
+            Console.WriteLine("[Main Thread] Please enter start URL:");
+            Console.Write("> ");
+            string starturl = Console.ReadLine();
+            Console.WriteLine(starturl);
+            links.Add(starturl);
+            Task.Run(() => manageThreads());
+            Console.WriteLine("Presse a key to leave.");
+            Console.ReadLine();
+        }
+
+        public static void manageThreads()
+        {
+            while (true)
+            {
+                if (currentthreads < 16 * Environment.ProcessorCount && currentindex <= links.Count - 1)
+                {
+                    Console.WriteLine("[Manage Thread] Start new request Thread. Start new Thread at Index: " +
+                                      currentindex);
+                    int x = currentindex;
+                    currentthreads++;
+                    Task.Run(() => requestThread(x));
+                    currentindex++;
+                }
+            }
+        }
+
+
+        public static async void requestThread(int index)
+        {
+            int th = currentthreads;
+            try
+            {
+                string url = links[index];
+                Console.WriteLine("[Request Thread "+th+"] URL: " + url);
+                HttpClient client = new HttpClient();
+                Uri uri = new Uri(url);
+                client.BaseAddress = uri;
+                var response = await client.GetAsync("");
+                string sresponse = await response.Content.ReadAsStringAsync();
+                foreach (Match m in Regex.Matches(sresponse, @"http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?"))
+                {
+                    if (!links.Contains(m.Value)) links.Add(m.Value);
+                }
+                currentthreads--;
+            }
+            catch (Exception)
+            {
+                currentthreads--;
+                Console.WriteLine("[Request Thread "+th+"] Error.");
+            }
+        }
+    }
+}
+
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("WebCrawlerDotNet")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebCrawlerDotNet")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("FA86ED12-9F87-4DA0-9A54-FC4E96A72BF9")]
+
